4bfecfe2a39982a89c25692082c2bc2f6e36a39d
René Martínez <hola@r3n0.com>
2023-10-05 16:53:10 -0500

    array y loop

--- sketch.js
+++ sketch.js
@@ -1,7 +1,17 @@
-function setup() {
-  createCanvas(windowWidth, windowHeight);
-}
+let miPelota;
+let frutas = ['mazana', miPelota, 234, 'granadilla'];
 
+function setup() {
+	createCanvas(windowWidth, windowHeight);
+	miPelota = new Pelota();
+}
+//     >>=>
 function draw() {
-  circle(mouseX, mouseY, 20);
+	background(255);
+	for (let x = 0; x < mouseX; x += mouseY + 10) {
+		square(x, 50, 5);
+	}
+	// circle(mouseX, mouseY, 20);
+	// miPelota.update(300);
+	// miPelota.display();
 }
